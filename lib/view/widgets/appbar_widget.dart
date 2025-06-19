3dc765b8dd4af27e6063c78003ab53c700a1eb63
Ghaith <ghayth.1900@gmail.com>
2025-06-19 12:05:23 +0200

    home page get started

--- lib/view/widgets/appbar_widget.dart
+++ lib/view/widgets/appbar_widget.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class AppbarWidget extends StatelessWidget {
-  const AppbarWidget({super.key});
+class PersonalAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final VoidCallback? onProfileTap;
+
+  const PersonalAppBar({
+    super.key,
+    required this.title,
+    this.onProfileTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      title: Text(
+        title,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20.sp,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      actions: [
+        IconButton(
+          onPressed: onProfileTap,
+          icon: CircleAvatar(
+            radius: 18.r,
+            backgroundImage: AssetImage(
+                'assets/images/profile.png'), // بدّلها لاحقًا بصورة المستخدم
+          ),
+        ),
+      ],
+    );
   }
+
+  @override
+  Size get preferredSize => Size.fromHeight(60.h);
 }
